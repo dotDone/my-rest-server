dbc7c3c054335731c5ad3b2d9186dcdd2993f7cd
dotDone <liam.simpson.91@gmail.com>
2022-02-16 12:59:51 +0800

    refactored everything

--- src/routes/auth.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import * as authController from '../controllers/auth.controller'
-
-const authRouter: Router = Router()
-
-authRouter.route('/').post(authController.login)
-
-export default authRouter
